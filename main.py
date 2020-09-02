5fddc30cc3572f493d37906344be55a57f7579d0
alvinmo <alvinmo@tencent.com>
2020-09-03 00:27:42 +0800

    feat(prepare_data): completed the generation and visualization of linearly-separable data

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-if '___name___' == 'main':
+# 项目部署：https://marlous.github.io/2019/04/11/Python-%E9%A1%B9%E7%9B%AE%E7%9A%84%E5%88%9B%E5%BB%BA%E5%BC%80%E5%8F%91%E3%80%81%E5%AE%89%E8%A3%85%E9%83%A8%E7%BD%B2%E9%97%AE%E9%A2%98/
+from prepare_data import DataProducer
+
+if __name__ == '__main__':
     n = 100
-    
+    patterns, targets = DataProducer.produce(n, True)
+    print(type(targets))
---
+++ prepare_data.py
@@ -0,0 +1,43 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+class DataProducer:
+    @classmethod
+    def produce(cls, n: int, plot: bool):
+        """
+        Produces two sets of points from multivariate normal distribution,
+        and shuffle samples to get one data set
+
+        :param n:
+            The number of samples per class
+        :param plot:
+            True if need to plot the data-set, false otherwise
+        :return:
+            A data-set that contains linearly-separable data for binary classification
+        """
+        mean_a = [3, 6]
+        mean_b = [-5, -2]
+        cov_a = [[0.5, 0], [0, 0.5]]
+        cov_b = cov_a
+        data_a = np.random.multivariate_normal(mean_a, cov_a, n)
+        data_b = np.random.multivariate_normal(mean_b, cov_b, n)
+
+        target_a = np.zeros(n)
+        target_b = np.ones(n)
+
+        data = np.append(data_a.T, data_b.T, axis=1)
+        target = np.append(target_a, target_b)
+
+        # shuffle the samples
+        permutation = np.random.permutation(data.shape[1])
+
+        data = data[:, permutation]
+        target = target[permutation]
+
+        if plot:
+            plt.scatter(data_a.T[0], data_a.T[1], edgecolors='b')
+            plt.scatter(data_b.T[0], data_b.T[1], edgecolors='r')
+            plt.show()
+
+        return data, target
