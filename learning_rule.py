feat(learning_rule): added the backprop rule and completed its implementation (sequential update) perf(prepare_data): set a fixed seed for the random number generation refactor(perceptron): upgrade the original layer to a multi-layer adapter

learning_rule.py
@@ -1,8 +1,16 @@
 import numpy as np
 
 
+def phi(x):
+    return (1 - np.exp(-x)) / (1 + np.exp(-x))
+
+
+def phi_d(x):
+    return (1 - phi(x) ** 2) / 2
+
+
 class LearningRule:
-    debug = False
+    debug = False   # debug option
 
     def fit_batch(self, W, X, T, eta, n_epoch):
         pass
@@ -10,9 +18,14 @@ class LearningRule:
     def fit_sequential(self, W, X, T, eta, n_epoch):
         pass
 
+    def predict(self, W: np.ndarray, x_test: np.ndarray):
+        n = x_test.shape[1]
+        x_test = np.row_stack((x_test, np.ones(n)))
+        prediction = np.sign(W.dot(x_test))
+        return prediction[0] if len(prediction.shape) == 1 else prediction
+
 
 class PerceptronRule(LearningRule):
-
     def fit_batch(self, W, X, T, eta, n_epoch):
         factor = 1 / (np.max(T) - np.min(T))
 
@@ -82,14 +95,13 @@ class DeltaRule(LearningRule):
     def fit_sequential(self, W, X, T, eta, n_epoch):
         # m features, n samples
         m, n = X.shape[0], X.shape[1]
-        output_dim = 1 if len(T.shape) == 1 else T.shape[0]
-        threshold = n * m * output_dim * 0.01
-        print("threshold, ", threshold)
+        out_dim = 1 if len(T.shape) == 1 else T.shape[0]
+        threshold = n * m * out_dim * 0.01
         for e in range(n_epoch):
             sum_error = 0.0
             for t in range(n):  # iterates over each sample
                 for i in range(m):  # iterates over each feature
-                    for j in range(output_dim):  # iterates over each output dimension
+                    for j in range(out_dim):  # iterates over each output dimension
                         x = X.T[t]  # sample: one-by-m array
                         activation = W[j].dot(x)
                         error = T[j][t] - activation
@@ -98,7 +110,85 @@ class DeltaRule(LearningRule):
                         sum_error += 0.5 * error ** 2
             print(">epoch=%s, learning rate=%s, error=%.2f" % (e, eta, sum_error))
             if sum_error < threshold:
-                print("\nTraining finished in %s epoch\n" % n_epoch)
+                print("\nTraining finished in %s epoch\n" % e)
+                break
+        return W
+
+
+class BackPropagation(LearningRule):
+    dim = 0  # the output dimension
+
+    def __init__(self, n_hidden_nodes):
+        self.n_hidden_nodes = n_hidden_nodes
+        self.n_layer = 2
+        self.W = None
+        self.V = None
+
+    def fit_sequential(self, W, X, T, eta, n_epoch):
+        # M features, N samples
+        M, N = X.shape[0], X.shape[1]
+        dim = 1 if len(T.shape) == 1 else T.shape[0]
+
+        W = np.random.normal(loc=0, scale=1, size=(self.n_hidden_nodes, M))
+        V = np.random.normal(loc=0, scale=1, size=(dim, self.n_hidden_nodes))
+        H = np.zeros((self.n_hidden_nodes, 1))
+        H_star = np.zeros(H.shape)
+        delta_h = np.zeros(H.shape)
+
+        O = np.zeros((dim, 1))
+        O_star = np.zeros(O.shape)
+        delta_o = np.zeros(O.shape)
+
+        threshold = N * M * dim * 0.001
+        for epoch in range(n_epoch):
+            sum_error = 0.0
+            for n in range(N):  # iterates over each sample
+                # forward pass
+                x = X.T[n]
+                t = T[:, n]
+                for k in range(self.n_hidden_nodes):
+                    w = W[k]
+                    H_star[k] = w.dot(x)
+                    H[k] = phi(w.dot(x))
+                    # print("Node ", k, x, w, H[k], t)
+                for k in range(dim):
+                    v = V[k]
+                    O_star[k] = v.dot(H)
+                    O[k] = phi(v.dot(H))
+                    sum_error += 0.5 * (t[k] - O[k]) ** 2
+
+                # backward pass
+                for k in range(dim):
+                    delta_o[k] = (O[k] - t[k]) * phi_d(O_star[k])
+
+                for j in range(self.n_hidden_nodes):
+                    delta_h[j] = (delta_o.dot(V[:, j])) * phi_d(H_star[j])
+
+                # weight update
+                for j in range(W.shape[0]):
+                    for i in range(W.shape[1]):
+                        W[j][i] += - eta * x[i] * delta_h[j]
+
+                for k in range(V.shape[0]):
+                    for j in range(V.shape[1]):
+                        V[k][j] += - eta * H[j] * delta_o[k]
+
+            print(">epoch=%s, learning rate=%s, error=%.2f" % (epoch, eta, sum_error))
+            if sum_error < threshold:
+                print("\nTraining finished in %s epoch\n" % epoch)
                 break
 
-        return W
+        self.W = W
+        self.V = V
+        self.dim = dim
+
+    def predict(self, W: np.ndarray, X: np.ndarray):
+        W, V = self.W, self.V
+        X = np.row_stack((X, np.ones(X.shape[1])))
+        H = W.dot(X)
+        H = [[phi(H[i][j]) for j in range(H.shape[1])] for i in range(H.shape[0])]
+        Output = V.dot(H)
+        Output = [[phi(Output[i][j]) for j in range(Output.shape[1])] for i in range(Output.shape[0])]
+
+        prediction = np.sign(np.array(Output))
+        return prediction[0] if prediction.shape[0] == 1 else prediction

perceptron.py
@@ -1,11 +1,11 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-from learning_rule import DeltaRule, PerceptronRule
+from learning_rule import DeltaRule, PerceptronRule, BackPropagation
 
 
-class SingleLayerPerceptron:
-    rule_map = {'perceptron': PerceptronRule(), 'delta': DeltaRule()}
+class Perceptron:
+    rule_map = {'perceptron': PerceptronRule(), 'delta': DeltaRule(), 'backprop': BackPropagation(n_hidden_nodes=20)}
 
     def __init__(self, eta: float = 0.01, algorithm: str = 'delta', debug: bool = False):
         # learning rate
@@ -55,7 +55,7 @@ class SingleLayerPerceptron:
         m, n = X.shape[0], X.shape[1]
 
         # add ones as the bias trick
-        W = self.init_weight(1, m + 1)
+        W = self.init_weight(self.out_dim, m + 1)
         X = np.row_stack((X, np.ones(n)))
         T = T.reshape(self.out_dim, n)
 
@@ -70,20 +70,17 @@ class SingleLayerPerceptron:
             self.plot_result(X, T, self.W)
 
     def predict(self, x_test: np.ndarray):
-        n = x_test.shape[1]
-        x_test = np.row_stack((x_test, np.ones(n)))
-        prediction = np.sign(self.W.dot(x_test))
-        return prediction[0] if self.out_dim == 1 else prediction
+        return self.rule.predict(self.W, x_test)
 
     def getWeights(self):
         return self.W[0] if self.out_dim == 1 else self.W
 
     @staticmethod
-    def plot_result(patterns, targets, w):
-        n = patterns.shape[1]
+    def plot_result(X, T, W):
+        n = X.shape[1]
 
-        class_0 = np.array([patterns.T[i] for i in range(n) if targets[0][i] == -1]).T
-        class_1 = np.array([patterns.T[i] for i in range(n) if targets[0][i] == 1]).T
+        class_0 = np.array([X.T[i] for i in range(n) if T[0][i] == -1]).T
+        class_1 = np.array([X.T[i] for i in range(n) if T[0][i] == 1]).T
         plt.title("Single layer perceptron learning")
 
         # plot the two classes
@@ -91,8 +88,8 @@ class SingleLayerPerceptron:
         plt.scatter(class_1[0], class_1[1], edgecolors='r', marker='x')
 
         # plot the decision boundary
-        x = np.linspace(min(patterns[0]), max(patterns[0]))
-        y = - (w[0][0] * x + w[0][2]) / w[0][1]
-        plt.plot(x, y)
+        x = np.linspace(min(X[0]), max(X[0]))
+        for i in range(W.shape[0]):
+            y = - (W[i][0] * x + W[i][2]) / W[i][1]
+            plt.plot(x, y)
         plt.show()
-

prepare_data.py
@@ -16,12 +16,17 @@ class DataProducer:
         :return:
             A data-set that contains linearly-separable data for binary classification
         """
-        mean_a = [-5, -2]
-        mean_b = [3, 6]
+        mean_a = [0, -2]
+        mean_b = [1, 1]
         cov_a = [[0.5, 0], [0, 0.5]]
         cov_b = cov_a
+
+        np.random.seed(0)
         data_a = np.random.multivariate_normal(mean_a, cov_a, n)
+
+        np.random.seed(1)
         data_b = np.random.multivariate_normal(mean_b, cov_b, n)
+
         data = np.append(data_a.T, data_b.T, axis=1)
 
         target_a = np.linspace(-1, -1, n)
@@ -29,6 +34,7 @@ class DataProducer:
         target = np.append(target_a, target_b)
 
         # shuffle the samples
+        np.random.seed(2)
         permutation = np.random.permutation(data.shape[1])
 
         data = data[:, permutation]